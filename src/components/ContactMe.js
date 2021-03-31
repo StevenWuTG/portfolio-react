7df300d4c26fc50a2fe5d14365dd24424d10f97c
StevenWuTG <stevenwu91@icloud.com>
2021-03-31 14:32:54 -0400

    contact form functions 100%

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,10 +1,37 @@
 import React from 'react'
-
+import emailjs from 'emailjs-com'
 const ContactMe = () => {
+
+    function sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('Gmail', 'template_aqdx4iw', e.target, 'user_9g8wroGHVK37aC43tSjIw')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          console.log(e)
+          e.target.reset()
+    }
+
     return (
-        <div className="contact-me">
-            <h1>COntact me plz</h1>
+        <div className="contact-me form-box">
             
+            <form className="contact-form" onSubmit={sendEmail}>
+            <input type="hidden" name="contact_number" />
+            <label>Subject:</label>
+            <input type="text" name="subject" />
+            <label>Name</label>
+            <input type="text" name="name" />
+            <label>Email</label>
+            <input type="email" name="email" />
+            <label>Message</label>
+            <textarea name="message" />
+            <input className="btn btn-primary"  type="submit" value="Send" />
+            </form>
+
+        
         </div>
     )
 }
