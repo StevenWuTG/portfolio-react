7a64525caa437c9a994f0ed07a5ed54571977225
StevenWuTG <stevenwu91@icloud.com>
2021-05-05 14:32:38 -0400

    centered contact form

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -16,22 +16,26 @@ const ContactMe = () => {
     }
 
     return (
-        <div className="contact-me form-box">
-            
-            <form className="contact-form" onSubmit={sendEmail}>
-            <input type="hidden" name="contact_number" />
-            <label>Subject:</label>
-            <input type="text" name="subject" />
-            <label>Name</label>
-            <input type="text" name="name" />
-            <label>Email</label>
-            <input type="email" name="email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input className="btn btn-primary"  type="submit" value="Send" />
-            </form>
 
-        
+        <div className="main-wrapper">
+
+            <div className="contact-me form-box">
+                
+                <form className="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Subject:</label>
+                <input type="text" name="subject" />
+                <label>Name</label>
+                <input type="text" name="name" />
+                <label>Email</label>
+                <input type="email" name="email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input className="btn btn-primary"  type="submit" value="Send" />
+                </form>
+
+            
+            </div>
         </div>
     )
 }
