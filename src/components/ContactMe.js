54744045b6ec1cf86c6ea46cbc5da85097ebbd8c
StevenWuTG <stevenwu91@icloud.com>
2021-05-08 19:57:59 -0400

    contactme done

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -20,22 +20,43 @@ const ContactMe = () => {
         <div className="main-wrapper">
 
             <div className="contact-me form-box">
+
+                <div className="about-icons">
+
+                    <a href="https://www.linkedin.com/in/stevenwubc/">
+                    <img src="https://img.icons8.com/fluent/240/000000/linkedin.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+
+                    <a href="https://github.com/StevenWuTG">
+                    <img src="https://img.icons8.com/fluent/240/000000/github.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+
+                    
+                    <a href="https://stevenwu91.medium.com/">
+                    <img src="https://img.icons8.com/ios-filled/250/000000/medium-monogram--v1.png" 
+                    alt="linkedin" align="left" width="60px" height="60"></img>
+                    </a>
+                    
+
+                </div>
                 
                 <form className="contact-form" onSubmit={sendEmail}>
-                <input type="hidden" name="contact_number" />
-                <label>Subject:</label>
-                <input type="text" name="subject" />
-                <label>Name</label>
-                <input type="text" name="name" />
-                <label>Email</label>
-                <input type="email" name="email" />
-                <label>Message</label>
-                <textarea name="message" />
-                <input className="btn btn-primary"  type="submit" value="Send" />
+                    <input type="hidden" name="contact_number" />
+                    <label>Subject:</label>
+                    <input type="text" name="subject" />
+                    <label>Name</label>
+                    <input type="text" name="name" />
+                    <label>Email</label>
+                    <input type="email" name="email" />
+                    <label>Message</label>
+                    <textarea name="message" />
+                    <input className="btn btn-primary"  type="submit" value="Send" />
                 </form>
-
-            
             </div>
+            
+                
         </div>
     )
 }
